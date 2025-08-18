0c90fcc285530f585e1923eb67254942cc18420c
N.Lukyanova <nadyalukyanovaqa@gmail.com>
2025-08-18 12:44:48 +0400

    bypassing footer ads

--- src/test/java/TestRegistrationForm.java
+++ src/test/java/TestRegistrationForm.java
@@ -3,14 +3,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class TestRegistrationForm {
     @BeforeAll
-    static void beforeAll() {
+    static void setupEnvironment() {
         Configuration.browserSize = "1800x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
@@ -18,7 +17,12 @@ public class TestRegistrationForm {
 
     @Test
     void registrationForm () {
+        //открыть браузер
         open("/automation-practice-form");
+
+        //обойти рекламу внизу страницы
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
 
         //Основная информация
         $("#firstName").setValue("John");
